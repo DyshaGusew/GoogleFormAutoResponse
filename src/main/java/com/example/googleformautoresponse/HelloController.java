05398bb514eb20e490aa2fba999bb22bfa6fff3e
DyshaGusew <pav060210@gmail.com>
2024-08-03 00:53:50 +1000

    Add and remove functions
    Create delete and add new answers pane. Full element position

--- src/main/java/com/example/googleformautoresponse/HelloController.java
+++ src/main/java/com/example/googleformautoresponse/HelloController.java
@@ -1,14 +1,55 @@
 package com.example.googleformautoresponse;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField urlInput;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField numAnswerInput;
+
+    @FXML
+    private ScrollPane placeQuestionScroll;
+
+    @FXML
+    private VBox placeQuestionVBox;
+
+
+    @FXML
+    protected void onAddQuestionButtonClick(){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("line-question.fxml"));
+            HBox newLoadedPane = loader.load();
+            placeQuestionVBox.getChildren().add(newLoadedPane);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    protected void onDeleteQuestionButtonClick() {
+        ObservableList<Node> children = placeQuestionVBox.getChildren();
+
+        if (children.size() != 0) {
+            children.remove(children.size() - 1);
+        }
+    }
+    @FXML
+    protected void onStartButtonClick() {
+
     }
 }
